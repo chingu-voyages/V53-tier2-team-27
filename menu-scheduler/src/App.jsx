9eb7c49ecc29d25ea56b9c658227e0b73dace86d
Matthew Neie <matneie@gmail.com>
2025-01-20 12:20:00 -0800

    start homepage

--- menu-scheduler/src/App.jsx
+++ menu-scheduler/src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
+import Home from "./components/Home";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1>Menu Scheduler</h1>
+      <Home />
     </>
   );
 }
