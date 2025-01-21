82a49378d4d75810ac26c1a42d0eda524f5c3266
goofy foot <williamk31@gmail.com>
2025-01-20 19:28:18 -0700

    allergy select form

--- menu-scheduler/src/App.jsx
+++ menu-scheduler/src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./App.css";
+import AllergyForm from "./components/Form/index";
 
 function App() {
   return (
     <>
       <h1>Menu Scheduler</h1>
+      <AllergyForm />
     </>
   );
 }
