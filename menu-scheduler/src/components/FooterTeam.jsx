f93c8326b170564e1c2bb150ff329ad33ef5a4bc
guillermoguevara887 <guillermoguevara887@gmail.com>
2025-01-22 15:38:43 -0800

    Adding FooterTeam component

---
+++ menu-scheduler/src/components/FooterTeam.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+const FooterTeam = () => {
+  return (
+    <div className="footer-container">
+      <p>This a Footer</p>
+
+    </div>
+
+  )
+};
+
+export default FooterTeam;
