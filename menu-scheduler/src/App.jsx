ee392d0fe7b0871b69948fe79e3af9ddcd1d185a
Matthew Neie <matneie@gmail.com>
2025-02-04 18:38:23 -0800

    allergies, calandar functions

--- menu-scheduler/src/App.jsx
+++ menu-scheduler/src/App.jsx
@@ -1,14 +1,26 @@
 import { useState } from "react";
-import Home from "./components/Home";
+import WeekCalendar from "./components/WeekCalender";
 import FooterTeam from "./components/FooterTeam";
 import Header from "./components/Header/Header";
+import "./styles/home.css";
+import "./styles/weekcalender.css";
 import "./App.css";
 
 function App() {
+  const [allergies, setAllergies] = useState([]);
+  const [menu, setMenu] = useState([]);
+
   return (
     <>
-      <Header />
-      <Home />
+      <Header allergies={allergies} setAllergies={setAllergies} />
+      <div className="home-container">
+        <WeekCalendar
+          allergies={allergies}
+          setAllergies={setAllergies}
+          menu={menu}
+          setMenu={setMenu}
+        />
+      </div>
       <FooterTeam />
     </>
   );
--- menu-scheduler/src/components/Header/Header.jsx
+++ menu-scheduler/src/components/Header/Header.jsx
@@ -1,23 +1,30 @@
 import React, { useState } from "react";
-import logo from './menuschedulericon 1.png'
-import logoTitle from './menuschedulertitle 1.png'
+import logo from "./menuschedulericon 1.png";
+import logoTitle from "./menuschedulertitle 1.png";
 import Modal from "../Modal/index";
-import './Header.css'
+import "./Header.css";
 
-const Header = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    return (
-        <div className="container">
-            <span>
-                <img src={logo} alt='Logo' className="img" />
-                <img src={logoTitle} alt='Logo' className="img" />
-            </span>
-            <button className="btn" onClick={() => setIsOpen(true)}>Adjust Allergy Preferences</button>
-            {isOpen && <Modal setIsOpen={setIsOpen} />}
-            {/* <button className="btn">Adjust Allergy Preferences</button> */}
-
-        </div>
-    )
-}
+const Header = ({ allergies, setAllergies }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="container">
+      <span>
+        <img src={logo} alt="Logo" className="img" />
+        <img src={logoTitle} alt="Logo" className="img" />
+      </span>
+      <button className="btn" onClick={() => setIsOpen(true)}>
+        Adjust Allergy Preferences
+      </button>
+      {isOpen && (
+        <Modal
+          setIsOpen={setIsOpen}
+          allergies={allergies}
+          setAllergies={setAllergies}
+        />
+      )}
+      {/* <button className="btn">Adjust Allergy Preferences</button> */}
+    </div>
+  );
+};
 
 export default Header;
